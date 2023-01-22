b0cfac64e90bdb1db990b3d2ce1657c6014b08f5
suhel khan <suhelkhan87514@gmail.com>
2023-01-23 01:36:46 +0530

    Some changes

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
 document.querySelector(".js-header-date").innerHTML = new Intl.DateTimeFormat("en-US", { month: "short", day: "numeric", year: "numeric" }).format(new Date);
 
-window.panelbear = window.panelbear || function () { (window.panelbear.q = window.panelbear.q || []).push(arguments) }, panelbear("config", { site: "LVrTyzS8YBv", spaMode: "history" });
 
