2f072b233c3c865a12daa52b99f9279f147aef84
suhel khan <suhelkhan87514@gmail.com>
2023-02-28 01:19:16 +0530

    Added loader

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,27 @@
 document.querySelector(".js-header-date").innerHTML = new Intl.DateTimeFormat("en-US", { month: "short", day: "numeric", year: "numeric" }).format(new Date);
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 
+script.onload = function () {
+    var preloaderFadeOutTime = 500;
+    var spinnerWrapper = $('.spinner-wrapper');
+
+    // Fade out the spinner after 5 seconds
+    setTimeout(function () {
+        spinnerWrapper.fadeOut(preloaderFadeOutTime);
+    }, 5000);
+};
+
+$(window).on('load', function () {
+    //Preloader
+    var preloaderFadeOutTime = 500;
+    var spinnerWrapper = $('.spinner-wrapper');
+
+    // Fade out the spinner after 5 seconds
+    setTimeout(function () {
+        spinnerWrapper.fadeOut(preloaderFadeOutTime);
+    }, 5000);
+});
